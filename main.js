542f1245c8dc2b32590696299945c6d383db1de5
Yahir Leo <123343914+Yiro13@users.noreply.github.com>
2023-01-30 22:56:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//let peso = document.getElementById('peso').value;
+//let peso2 = parseFloat(peso);
+
+function calcular(){
+    let peso = document.getElementById('peso').value;
+    let peso2 = parseFloat(peso);
+    alert("Pesas " + peso2 + " kilogramos");
+}
+
+
